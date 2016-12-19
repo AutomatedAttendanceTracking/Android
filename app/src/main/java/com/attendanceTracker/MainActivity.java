b69efee59652a48789ae7b60b2aa212471562a1e
Thomas Mauerer <thomas.mauerer@tum.de>
2016-12-19 13:27:05 +0100

    basic refactoring

--- app/src/main/java/com/attendanceTracker/MainActivity.java
+++ app/src/main/java/com/attendanceTracker/MainActivity.java
@@ -1,16 +1,14 @@
-package com.example.myfirstapp;
+package com.attendanceTracker;
 
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
 import android.widget.Toast;
+
+import com.example.attendanceTracker.R;
 
 import org.restlet.resource.ClientResource;
 import org.w3c.dom.Document;
@@ -19,7 +17,6 @@ import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
 import java.io.ByteArrayInputStream;
-import java.io.IOException;
 import java.io.InputStream;
 
 import javax.xml.parsers.DocumentBuilder;
@@ -30,70 +27,40 @@ import javax.xml.xpath.XPathFactory;
 
 public class MainActivity extends AppCompatActivity {
 
-
-    Button requestButton;
-    TextView view3;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        /**requestButton = (Button) findViewById(R.id.req_button);
-
-        requestButton.setOnClickListener(new View.OnClickListener(){
-
-            @Override
-           public void onClick(View view) {
-                String url = "http://ase2016-148507.appspot.com/rest/guestbook/";
-                new netWorkRequestTask(url).execute();
-            }
-        });**/
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main,menu);
+        getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
-
-
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-
-        int id = item.getItemId();
-
-        switch (id) {
+        switch (item.getItemId()) {
             case R.id.helloworld_menu:
-
-                Toast.makeText(getApplicationContext(),"Hello World", Toast.LENGTH_LONG).show();
-               TextView view1 = (TextView)findViewById(R.id.textView);
-                view1.setText("Hello World!");
+                Toast.makeText(getApplicationContext(), "Hello World", Toast.LENGTH_LONG).show();
                 return true;
 
             case R.id.helloase_menu:
-                Toast.makeText(getApplicationContext(),"Hello Ase", Toast.LENGTH_LONG).show();
-                TextView view2 = (TextView)findViewById(R.id.textView);
-                view2.setText("Hello Ase!");
+                Toast.makeText(getApplicationContext(), "Hello Ase", Toast.LENGTH_LONG).show();
                 return true;
-            case R.id.guestbook_menu:
-                Toast.makeText(getApplicationContext(),"Guestbook", Toast.LENGTH_LONG).show();
-                String url = "http://ase2016-148507.appspot.com/rest/guestbook/";
-                new netWorkRequestTask(url).execute();
-                view3 = (TextView)findViewById(R.id.textView);
-                return true;
-        }
 
-        return onOptionsItemSelected(item);
+            default: return false;
+        }
     }
 
 
-
-    public class netWorkRequestTask extends AsyncTask <Void,Void,Boolean>{
+    public class NetWorkRequestTask extends AsyncTask<Void, Void, Boolean> {
 
         private final String url;
         private String response;
-        public netWorkRequestTask(String url) {
+
+        public NetWorkRequestTask(String url) {
             this.url = url;
         }
 
@@ -126,8 +93,8 @@ public class MainActivity extends AppCompatActivity {
                     Node node = nodeList.item(i);
                     Element eElement = (Element) node;
 
-                    stringBuilder.append("E-Mail: "+(String) eElement.getElementsByTagName("email").item(0).getTextContent()+"\r\n");
-                    stringBuilder.append("Content: " + (String) eElement.getElementsByTagName("content").item(0).getTextContent()+"\n\n");
+                    stringBuilder.append("E-Mail: " + (String) eElement.getElementsByTagName("email").item(0).getTextContent() + "\r\n");
+                    stringBuilder.append("Content: " + (String) eElement.getElementsByTagName("content").item(0).getTextContent() + "\n\n");
 
                     //content =+ "E-Mail: \n" + (String) eElement.getElementsByTagName("email").item(0).getTextContent()+"\n";
                     //content =+ "Content: \n" + (String) eElement.getElementsByTagName("content").item(0).getTextContent()+"\n";
@@ -135,23 +102,20 @@ public class MainActivity extends AppCompatActivity {
                 }
                 content = stringBuilder.toString();
                 this.response = content;
-                Log.d("success",this.response);
+                Log.d("success", this.response);
                 return true;
             } catch (Exception exc) {
-                Log.e("error",exc.getMessage());
+                Log.e("error", exc.getMessage());
                 return false;
             }
         }
 
         @Override
         protected void onPostExecute(final Boolean success) {
-            if(success) {
-                Toast.makeText(getApplicationContext(),"Success", Toast.LENGTH_LONG).show();
-                view3.setText(response);
-            }
-            else {
-                Toast.makeText(getApplicationContext(),"Failed to get server response!", Toast.LENGTH_LONG).show();
-                view3.setText("Failed to get server response");
+            if (success) {
+                Toast.makeText(getApplicationContext(), "Success", Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(getApplicationContext(), "Failed to get server response!", Toast.LENGTH_LONG).show();
             }
         }
     }
--- app/src/test/java/com/attendanceTracker/ExampleUnitTest.java
+++ app/src/test/java/com/attendanceTracker/ExampleUnitTest.java
@@ -1,4 +1,4 @@
-package com.example.myfirstapp;
+package com.attendanceTracker;
 
 import org.junit.Test;
 
